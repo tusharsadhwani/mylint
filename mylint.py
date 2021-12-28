02bc03aee13881fcafbba450cc49eaff8e36b83c
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-12-29 01:32:45 +0530

    Add another checker

--- mylint.py
+++ mylint.py
@@ -4,62 +4,143 @@ import sys
 from typing import NamedTuple
 
 
+class Violation(NamedTuple):
+    """
+    Every rule violation contains a node that breaks the rule,
+    and a message that will be shown to the user.
+    """
+
+    node: ast.AST
+    message: str
+
+
 class Checker(ast.NodeVisitor):
-    def __init__(self):
-        self.violating_nodes = set()
+    """
+    A Checker is a Visitor that defines a lint rule, and stores all the
+    nodes that violate that lint rule.
+    """
 
-
-class LintRule(NamedTuple):
-    issue_code: str
-    issue_msg: int
-    checker: Checker
+    def __init__(self, issue_code):
+        self.issue_code = issue_code
+        self.violations = set()
 
 
 class Linter:
-    def __init__(self):
-        self.lint_rules = set()
+    """Holds all list rules, and runs them against a source file."""
 
-    def add_rule(self, issue_code, issue_msg, checker):
-        self.lint_rules.add(LintRule(issue_code, issue_msg, checker))
+    def __init__(self):
+        self.checkers = set()
 
     @staticmethod
-    def print_violation(file_name, node, issue_code, issue_msg):
-        print(
-            f"{file_name}:{node.lineno}:{node.col_offset}: "
-            f"{issue_code}: {issue_msg}"
-        )
+    def print_violations(checker, file_name):
+        for node, message in checker.violations:
+            print(
+                f"{file_name}:{node.lineno}:{node.col_offset}: "
+                f"{checker.issue_code}: {message}"
+            )
 
     def run(self, source_path):
+        """Runs all lints on a source file."""
         file_name = os.path.basename(source_path)
 
         with open(source_path) as source_file:
             source_code = source_file.read()
 
         tree = ast.parse(source_code)
-        for issue_code, issue_msg, checker in self.lint_rules:
+        for checker in self.checkers:
             checker.visit(tree)
-            for node in checker.violating_nodes:
-                self.print_violation(file_name, node, issue_code, issue_msg)
+            self.print_violations(checker, file_name)
 
 
 class SetDuplicateItemChecker(Checker):
     """Checks if a set in your code has duplicate constants."""
 
-    def visit_Set(self, node: ast.Set):
-        if len(node.elts) > 3:
-            self.violating_nodes.add(node)
+    def visit_Set(self, node):
+        """Stores all the constants this set holds, and finds duplicates"""
+        seen_values = set()
+        for element in node.elts:
+            # We're only concerned about constant values like ints.
+            if not isinstance(element, ast.Constant):
+                continue
+
+            # if it's already in seen values, raise a lint violation.
+            value = element.value
+            if value in seen_values:
+                violation = Violation(
+                    node=element,
+                    message=f"Set contains duplicate item: {value!r}",
+                )
+                self.violations.add(violation)
+
+            else:
+                seen_values.add(element.value)
 
 
-if __name__ == "__main__":
+class UnusedVariableInScopeChecker(Checker):
+    """Checks if any variables are unused in this node's scope."""
+
+    def __init__(self, issue_code):
+        super().__init__(issue_code)
+        # unused_names is a dictionary that stores variable names, and
+        # whether or not they've been found in a "Load" context yet.
+        # If it's found to be used, its value is turned to False.
+        self.unused_names = {}
+
+        # name_nodes holds the first occurences of variables.
+        self.name_nodes = {}
+
+    def visit_Name(self, node):
+        """Find all nodes that only exist in `Store` context"""
+        var_name = node.id
+
+        if isinstance(node.ctx, ast.Store):
+            # If it's a new name, save the node for later
+            if var_name not in self.name_nodes:
+                self.name_nodes[var_name] = node
+
+            # If we've never seen it before, it is unused.
+            if var_name not in self.unused_names:
+                self.unused_names[var_name] = True
+
+        else:
+            # It's used somewhere.
+            self.unused_names[var_name] = False
+
+
+class UnusedVariableChecker(Checker):
+    def check_for_unused_variables(self, node):
+        """Find unused variables in the local scope of this node."""
+        visitor = UnusedVariableInScopeChecker(self.issue_code)
+        visitor.visit(node)
+
+        for name, unused in visitor.unused_names.items():
+            if unused:
+                node = visitor.name_nodes[name]
+                self.violations.add(Violation(node, f"Unused variable: {name!r}"))
+
+    def visit_Module(self, node):
+        self.check_for_unused_variables(node)
+        super().generic_visit(node)
+
+    def visit_ClassDef(self, node):
+        self.check_for_unused_variables(node)
+        super().generic_visit(node)
+
+    def visit_FunctionDef(self, node):
+        self.check_for_unused_variables(node)
+        super().generic_visit(node)
+
+
+def main():
     source_paths = sys.argv[1:]
 
     linter = Linter()
-
-    linter.add_rule(
-        issue_code="W001",
-        issue_msg="Set contains duplicate items",
-        checker=SetDuplicateItemChecker(),
-    )
+    linter.checkers.add(SetDuplicateItemChecker(issue_code="W001"))
+    linter.checkers.add(UnusedVariableChecker(issue_code="W002"))
 
     for source_path in source_paths:
         linter.run(source_path)
+
+
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 s = {1, 2}
-s2 = {1, 2, 3, 4}
-
-t = [1, 2, 3]
-
+l = [1, 2, 3, 1, 2, 3]
 
 def main():
-    for item in t:
-        s3 = {1, 10, 100, 1000}
-        print(item)
+    var = [1, 2, 3]
+    for item in l:
+        methods = {
+            "GET",
+            "PUT",
+            "POST",
+            "DELETE",
+            "PUT",
+        }
+        if item in methods:
+            print(item)
+
+s2 = {1, 2, 3, 1}
+var = True
+print(var)
